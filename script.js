ed665ffef6e9513dc3dfab4fabadbe515da780f1
sonomastea <sbstea@usf.edu>
2024-06-19 15:24:56 -0400

    Configure linear scale for bar width

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const data= [100, 420 , 230, 850, 560, 925];
 
+///dimensions
+const width= 500;
+const barHeight= 20;
+const barMargin= 1;
+const height= data.length * (barHeight + barMargin);
+
+//SVG Container
+const svg= d3.select("body")
+.append("svg")
+.attr("width", width)
+.attr("height",height);
+
+
+//linear scale for bar width
+
+const xScale= d3.scaleLinear ()
+.domain([0, d3.max(data)])
+.range([50,width]);
